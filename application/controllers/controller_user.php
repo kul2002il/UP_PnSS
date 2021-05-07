9a86f6815d33f8ee2bd05215054b2352abd2e50e
Ilia <kul.2002.il@gmail.com>
2021-05-07 17:07:01 +0700

    Начало работы с пользователем.

---
+++ application/controllers/controller_user.php
@@ -0,0 +1,15 @@
+<?php
+
+class Controller_User extends Controller
+{
+	/*
+	function action_index()
+	{
+		$this->view->generate('view_error404.php');
+	}
+	*/
+	function action_login()
+	{
+		$this->view->generate('view_error404.php');
+	}
+}
