2ba897387366c16f38d13c3c40ac1c15da9ca70b
Ilia <kul.2002.il@gmail.com>
2021-05-09 16:50:24 +0700

    Добавлено добавление, редактирование и удаление проектов портфолио.

--- application/controllers/controller_portfolio.php
+++ application/controllers/controller_portfolio.php
@@ -1,5 +1,4 @@
-
-	parent::__construct();<?php
+<?php
 
 class Controller_Portfolio extends Controller
 {
@@ -13,5 +12,68 @@ class Controller_Portfolio extends Controller
 	{
 		$data = $this->model->getData();
 		$this->view->generate('view_portfolio.php', $data);
+	}
+
+	public function action_add()
+	{
+		if (!isset($_SESSION["user"]))
+		{
+			header('Location: /user/login');
+			return;
+		}
+
+		global $messages;
+		if (isset($_POST["addProject"]))
+		{
+			$res = $this->model->add($_POST);
+			if($res === true)
+			{
+				header('Location: /portfolio');
+				return;
+			}
+			array_push($messages, $res);
+		}
+		$this->view->generate('view_portfolio_add.php');
+	}
+
+	public function action_edit($index = null)
+	{
+		if (!$index)
+		{
+			header('Location: /portfolio/add');
+			return;
+		}
+
+		global $messages;
+		if (isset($_POST["editProject"]))
+		{
+			$res = $this->model->edit($_POST, $index);
+			if($res === true)
+			{
+				header('Location: /portfolio');
+				return;
+			}
+			array_push($messages, $res);
+		}
+
+		if (isset($_POST["deleteProject"]))
+		{
+			$res = $this->model->delete($index);
+			if($res === true)
+			{
+				header('Location: /portfolio');
+				return;
+			}
+			array_push($messages, $res);
+		}
+
+		$data = $this->model->getProjectData($index);
+		if(is_string($data))
+		{
+			array_push($messages, $data);
+			$data = [];
+		}
+
+		$this->view->generate('view_portfolio_edit.php', $data);
 	}
 }
--- application/views/view_test.php
+++ application/views/view_test.php
@@ -1,7 +1,31 @@
 <h1>Страница для тестов</h1>
+
+<table>
+
 <?php
-global $passwordSalt;
-?>
-sudo  - <?= crypt("sudo" , $passwordSalt) ?> <br>
-admin - <?= crypt("admin", $passwordSalt) ?> <br>
-user  - <?= crypt("user" , $passwordSalt) ?> <br>
+
+$urls = [
+	"https://ru.stack.com/",
+	"https://stack.com",
+	"ru.stack.com/",
+	"stack.com/",
+	"https://ru.stack.com",
+	"stack.com",
+
+	"stack..com",
+	"sta..ck.com",
+	"stack.comonfrom",
+];
+
+foreach ($urls as $url) { ?>
+	<tr>
+		<td>
+			<?= $url ?>
+		</td>
+		<td>
+			<?= preg_match('#^(http://|https://)?([0-9a-zA-Zа-яА-ЯёЁ]+\.){1,2}[0-9a-zA-Zа-яА-ЯёЁ]{2,6}/?$#', $url) ?>
+		</td>
+	</tr>
+<?php } ?>
+
+</table>
