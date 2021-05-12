ac0036ce8626b8226bf87ac49a920e8e84302294
Ilia <kul.2002.il@gmail.com>
2021-05-12 19:43:46 +0700

    Сдан 2 день.

--- Doc/index.php
+++ Doc/index.php
@@ -60,7 +60,7 @@ $files = getBranch("..");
 
 			<p class="title_work">
 				"Программирование на стороне сервера"<br/>
-				Неделя 1. День 1.
+				Неделя 1.
 			</p>
 
 			<div class="signature">
@@ -104,10 +104,10 @@ $files = getBranch("..");
 		<div>
 			<h2>Ход работы</h2>
 
-			<h3>Схема работы MVC модели</h3>
+			<h3>ERD диаграмма БД</h3>
 			<p>
 				<div class="img">
-					<img src="img/Classes_v0.1.svg">
+					<img src="img/EDR.png">
 					<p>Диаграмма классов</p>
 				</div>
 			</p>
